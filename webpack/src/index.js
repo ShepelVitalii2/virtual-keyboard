8ad89b94082db33212f530e07e448d0f32d89096
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2021-02-17 23:32:59 +0200

    1 draft of virtual keyboard

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,19 +1,78 @@
-// import example from "./images/example.svg";
 import "./style.css";
 
-class Game {
-  name = "Violin Charades";
+const keyboard = [
+  113,
+  119,
+  101,
+  114,
+  116,
+  121,
+  117,
+  105,
+  111,
+  112,
+  91,
+  93,
+  97,
+  115,
+  100,
+  102,
+  103,
+  104,
+  106,
+  107,
+  108,
+  59,
+  39,
+  122,
+  120,
+  99,
+  118,
+  98,
+  110,
+  109,
+  44,
+  46,
+  47,
+];
+document.onkeypress = function (event) {
+  keyboard.push(event.charCode);
+  console.log(keyboard);
+};
+
+function init() {
+  let out = "";
+  for (let i = 0; i < keyboard.length; i += 1) {
+    if (i === 12 || i === 24) {
+      out += '<div class="clearfix"></div>';
+    }
+    out +=
+      '<div class="k-key" data="' +
+      keyboard[i] +
+      '"> ' +
+      String.fromCharCode(keyboard[i]) +
+      "</div>";
+  }
+  document.querySelector("#root").innerHTML = out;
 }
-const myGame = new Game();
+init();
 
-// создаем параграф
-const p = document.createElement("p");
-p.textContent = `I like ${myGame.game}.`;
+document.onkeypress = function (e) {
+  document.querySelectorAll("#root .k-key").forEach(function (element) {
+    element.classList.remove("active");
+  });
+  document
+    .querySelector('#root .k-key[data="' + e.keyCode + '"]')
+    .classList.add("active");
+};
 
-// создаем элемент заголовка
-const heading = document.createElement("h1");
-heading.textContent = "Как интересно!";
-
-// добавляем параграф и заголовок в DOM
-const root = document.querySelector("#root");
-root.append(heading, p);
+document.querySelectorAll("#root .k-key").forEach(function (element) {
+  element.onclick = function (event) {
+    document.querySelectorAll("#root .k-key").forEach(function (element) {
+      element.classList.remove("active");
+    });
+    let code = this.getAttribute("data");
+    this.classList.add("active");
+    // console.log(code);
+  };
+});
